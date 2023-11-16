209c8e9293bc679749d95d2f9a271c71bfdaf865
Ben Trebilcock <trebi018@umn.edu>
2023-11-16 13:39:02 -0600

    Added links to portfolio images.

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './portfolio.css';
 import portfolio1 from '../../assets/portfolio1.png'
 import portfolio2 from '../../assets/portfolio2.png'
@@ -8,19 +8,25 @@ const Portfolio = ()  =>    {
   return  (
     <section id="portfolio">
       <h2 className="portfolioTitle">My Portfolio</h2>
-      <span className="portfolioDesc">Below you can find some of my previous works:</span>
+      <span className="portfolioDesc">Click on the images below to view previous works:</span>
       <div className="portfolioImgs">
         <div className="hometownClassifieds">
         <h2>Home Town Classifieds</h2>
-        <img src={portfolio1} alt="" className="portfolioImg" />
+        <a href="https://github.com/Aidan-Windebank/hometown-classifieds" target="_blank">
+        <img src={portfolio1} alt="portfolio1" className="portfolioImg" />
+        </a>
         </div>
         <div className="shazingle">
         <h2>Shazingle</h2>
-        <img src={portfolio2} alt="" className="portfolioImg" />
+        <a href="https://github.com/toxicmix/group-project" target="_blank">
+        <img src={portfolio2} alt="portfolio2" className="portfolioImg" />
+        </a>
         </div>
         <div className="thinkSync">
         <h2>ThinkSync</h2>
-        <img src={portfolio3} alt="" className="portfolioImg" />
+        <a href="https://github.com/BenTrebi/ThinkSync" target="_blank">
+        <img src={portfolio3} alt="portfolio3" className="portfolioImg" />
+        </a>
         </div>
       </div>
       <button className="portfolioBtn">See More</button>
