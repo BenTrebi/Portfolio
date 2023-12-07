fd10aa429dad48399042d07df662f770251f76de
Ben Trebilcock <trebi018@umn.edu>
2023-12-07 13:51:08 -0600

    Added view more button and portfolio card.

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import './portfolio.css';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,9 +9,27 @@ import CardContent  from "@mui/material/CardContent";
 import portfolio1 from '../../assets/portfolio1.png'
 import portfolio2 from '../../assets/portfolio2.png'
 import portfolio3 from '../../assets/portfolio3.png'
+import portfolio4 from '../../assets/portfolio4.png'
 import Button from "@mui/material/Button";
 
 const Portfolio = ()  =>    {
+
+  const useToggle = (initialState) => {
+    const [toggleValue, setToggleValue] = useState(initialState);
+
+    const toggler = () => { setToggleValue(!toggleValue) };
+    return [toggleValue, toggler]
+  };
+
+  const [toggle, setToggle] = useToggle();
+
+  const [buttonText, setButtonText] = useState('View More'); 
+  
+    const handleClick = () => { 
+        setButtonText(buttonText === 'View More' ? 'View Less' : 'View More'); 
+    }; 
+  
+
   return  (
     <section id="portfolio">
       <h2 className="portfolioTitle">My Portfolio</h2>
@@ -58,6 +77,27 @@ const Portfolio = ()  =>    {
             <Button variant='outlined' size="small" href="https://github.com/BenTrebi/ThinkSync" target="_blank">View</Button>
           </CardActions>
         </Card>
+        {toggle && (
+        <Card sx={{ maxWidth: 1000 }} className="card">
+          <CardMedia component="img" alt="sharkQuiz" height="250" src={portfolio4} className="cardImg"/>
+          <CardContent className="cardContent">
+            <Typography gutterBottom variant="h5" component='div' fontWeight={600} fontFamily='Poppins, sans-serif'>
+              Shark Quiz
+            </Typography>
+            <Typography variant="body1"  fontWeight={100} fontSize={15} fontFamily='Poppins, sans-serif'>
+            This quiz application was built as a passion project in order to better understand React. 
+            It was built using React, Javascript, and CSS.
+            </Typography>
+          </CardContent>
+          <CardActions className="cardBtn">
+            <Button variant='outlined' size="small" href="https://github.com/BenTrebi/shark-quiz" target="_blank">View</Button>
+          </CardActions>
+        </Card>
+        )}
+        <Button variant="outlined" size="medium" className="moreBtn" onClick={() => {
+          setToggle();
+          handleClick();
+        }}>{buttonText}</Button>
     </section>
   );
 }
