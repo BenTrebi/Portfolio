33884bb1bcc75399951cafc4a31f538f9d986a79
Ben Trebilcock <trebi018@umn.edu>
2024-01-17 10:37:33 -0600

    Minor changes to appearance.

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -34,6 +34,21 @@ const Portfolio = ()  =>    {
     <section id="portfolio">
       <h2 className="portfolioTitle">My Portfolio</h2>
       <span className="portfolioDesc">Click the view button to see previous works.</span>
+      <Card sx={{ maxWidth: 1000 }} className="card">
+          <CardMedia component="img" alt="thinkSync" height="250" src={portfolio3} className="cardImg"/>
+          <CardContent className="cardContent">
+            <Typography gutterBottom variant="h5" component='div' fontWeight={600} fontFamily='Poppins, sans-serif'>
+              ThinkSync
+            </Typography>
+            <Typography variant="body1"  fontWeight={100} fontSize={15} fontFamily='Poppins, sans-serif'>
+            ThinkSync is a tool designed to help a team collaborate or an individual come to a decision. Anyone can use ThinkSync to dynamically come to a decision, however by signing up, users can save and review their data which opens the possibility of analyzing trends or recurring choices.
+            It is built using React, MongoDB, Mongoose, Express, and Material Design for Bootstrap v5 CSS framework.
+            </Typography>
+          </CardContent>
+          <CardActions className="cardBtn">
+            <Button variant='outlined' size="small" href="https://github.com/BenTrebi/ThinkSync" target="_blank">View</Button>
+          </CardActions>
+        </Card>
         <Card sx={{ maxWidth: 1000 }} className="card">
           <CardMedia component="img" alt="hometown classifieds" height="250" src={portfolio1} className="cardImg"/>
           <CardContent className="cardContent">
@@ -62,21 +77,6 @@ const Portfolio = ()  =>    {
             <Button variant='outlined' size="small" href="https://github.com/toxicmix/group-project" target="_blank">View</Button>
           </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 1000 }} className="card">
-          <CardMedia component="img" alt="thinkSync" height="250" src={portfolio3} className="cardImg"/>
-          <CardContent className="cardContent">
-            <Typography gutterBottom variant="h5" component='div' fontWeight={600} fontFamily='Poppins, sans-serif'>
-              ThinkSync
-            </Typography>
-            <Typography variant="body1"  fontWeight={100} fontSize={15} fontFamily='Poppins, sans-serif'>
-            ThinkSync is a tool designed to help a team collaborate or an individual come to a decision. Anyone can use ThinkSync to dynamically come to a decision, however by signing up, users can save and review their data which opens the possibility of analyzing trends or recurring choices.
-            It is built using React, MongoDB, Mongoose, Express, and Material Design for Bootstrap v5 CSS framework.
-            </Typography>
-          </CardContent>
-          <CardActions className="cardBtn">
-            <Button variant='outlined' size="small" href="https://github.com/BenTrebi/ThinkSync" target="_blank">View</Button>
-          </CardActions>
-        </Card>
         {toggle && (
         <Card sx={{ maxWidth: 1000 }} className="card">
           <CardMedia component="img" alt="sharkQuiz" height="250" src={portfolio4} className="cardImg"/>
@@ -85,7 +85,7 @@ const Portfolio = ()  =>    {
               Shark Quiz
             </Typography>
             <Typography variant="body1"  fontWeight={100} fontSize={15} fontFamily='Poppins, sans-serif'>
-            This quiz application was built as a passion project in order to better understand React. 
+            This quiz application was built as a personal project in order to better understand React. 
             It was built using React, Javascript, and CSS.
             </Typography>
           </CardContent>
